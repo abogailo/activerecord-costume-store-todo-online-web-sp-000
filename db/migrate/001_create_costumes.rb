b1ac255b91bac42bee73a891a6c1f2450dc76bf8
Abagail Hudson <abagailanne@gmail.com>
2019-07-11 23:00:03 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -11,9 +11,10 @@ class CreateCostumes < ActiveRecord::Migration[5.0]
   def change
     create_table :costumes do |t|
       t.string :name
-      t.price :price
-      t.size :size
-      t.image_url :image_url
+      t.integer :price
+      t.string :size
+      t.string :image_url
+      t.timestamps
     end
   end
 end
