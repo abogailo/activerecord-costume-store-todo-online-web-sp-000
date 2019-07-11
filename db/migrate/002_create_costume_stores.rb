bd2b6a7aab7c78a05a3264e48e6e1975e156da4a
Abagail Hudson <abagailanne@gmail.com>
2019-07-11 23:05:48 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,14 @@
 # Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[5.0]
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :inventory
+      t.integer :num_employees
+      t.boolean :still_open
+      t.string :opening_time
+      t.string :closing_time
+    end
+  end
+end
